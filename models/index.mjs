ec79b20335759ed14dc35c87ffe7d3adfaebb145
Sebastian Castro <jsebascp6@gmail.com>
2024-08-21 19:24:45 -0500

    run prettier

--- models/index.mjs
+++ models/index.mjs
@@ -17,12 +17,16 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
@@ -30,13 +34,13 @@ fs
       file.indexOf('.test.mjs') === -1 // Cambiado de '.test.js' a '.test.mjs'
     );
   })
-  .forEach(async file => {
+  .forEach(async (file) => {
     const modelModule = await import(path.join(__dirname, file));
     const model = modelModule.default(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
